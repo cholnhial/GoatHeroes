f499bfffdb80e9fdfc657ce4cff88995ab28ce98
Chol Nhial <chol.nhial.chol@gmail.com>
2022-05-17 20:24:20 +1000

    javascript for testimonials

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+/**
+ * Used to get the data object containing testmonials
+ * etc.
+ *
+ * @returns {Promise<Response>} the data retrieved
+ */
+async function getData() {
+    return fetch('/data/data.json')
+        .then(async function(resp) {
+            return await resp.json();
+        });
+}
 
+/**
+ *  Connect event handlers for testimonials on controls
+ *  on home page.
+ */
 function handleTestimonialControls () {
     $('#tbutton-next').on('click', function() {
         console.log("Clicked next");
@@ -9,7 +25,7 @@ function handleTestimonialControls () {
 }
 $(document).ready(() => {
 
-    // connect events
+    // connect events for testimonial controls
     handleTestimonialControls();
 
 });
